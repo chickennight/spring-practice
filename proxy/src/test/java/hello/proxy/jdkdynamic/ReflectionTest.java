7276c292dc2a261f7dc663091f3c48f68f6ebbc9
이종민 <john0513@naver.com>
2024-07-02 15:27:09 +0900

    ãaspect

--- proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -3,7 +3,6 @@ package hello.proxy.jdkdynamic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Slf4j
